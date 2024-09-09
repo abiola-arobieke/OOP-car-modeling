8bc4f01777a247ab836b7c6fb4f2bc6328ac93c8
abiola <abiola.arobieke@gmail.com>
2024-09-09 12:49:55 -0700

    Add car_model.js file to the project

---
+++ car_modeling/car_model/car_model.js
@@ -0,0 +1,19 @@
+export class CarModel {
+    constructor(title){
+        this.title = title
+        this.cars = []
+    }
+
+    get car(){
+        return cars
+    }
+
+    set car(vehicle){
+        this.cars = vehicle
+    }
+
+    add_car(vehicle){
+        this.cars.push(vehicle)
+        vehicle.car_model = this
+    }
+}
