31af3767ea0564e74241c82c38a3fceb08c8aa08
abiola <abiola.arobieke@gmail.com>
2024-09-10 07:18:44 -0700

    Add a method to apply brake

--- car_modeling/car/brake.js
+++ car_modeling/car/brake.js
@@ -1,11 +1,14 @@
 export class Brake {
-    constructor(brakeType, car, wheel){
+    constructor(brakeType, car, wheel) {
         this.brakeType = brakeType
         this.car = car
         this.wheel = wheel
 
         car.brake = this
         wheel.brake = this
+    }
 
+    apply() {
+        return this.car.engine.decelerate()
     }
 }
