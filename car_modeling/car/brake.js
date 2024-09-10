6f1ebcb0fe6936c9d014369cb4b004bef53c3f85
abiola <abiola.arobieke@gmail.com>
2024-09-10 04:54:39 -0700

    Add brake.js file to the project

---
+++ car_modeling/car/brake.js
@@ -0,0 +1,11 @@
+export class Brake {
+    constructor(brakeType, car, wheel){
+        this.brakeType = brakeType
+        this.car = car
+        this.wheel = wheel
+
+        car.brake = this
+        wheel.brake = this
+
+    }
+}
