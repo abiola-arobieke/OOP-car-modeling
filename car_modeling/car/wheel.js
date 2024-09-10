cc5035aed0316c25510bc5270f786a3b7aac3912
abiola <abiola.arobieke@gmail.com>
2024-09-10 07:17:39 -0700

    Add instance field to the class

--- car_modeling/car/wheel.js
+++ car_modeling/car/wheel.js
@@ -1,7 +1,7 @@
 import { Tire } from "./tire.js";
 
 export class Wheel extends Tire {
-    constructor(diameter, width, air_pressure){
+    constructor(diameter, width, air_pressure) {
         super(width, air_pressure)
         this.diameter = diameter
         this.suspension;
