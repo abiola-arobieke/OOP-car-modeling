d26e86e996112f67737c5731bcd4c251d03ddcfb
abiola <abiola.arobieke@gmail.com>
2024-09-10 04:55:41 -0700

    Add wheel.js file to the project

---
+++ car_modeling/car/wheel.js
@@ -0,0 +1,10 @@
+import { Tire } from "./tire.js";
+
+export class Wheel extends Tire {
+    constructor(diameter, width, air_pressure){
+        super(width, air_pressure)
+        this.diameter = diameter
+        this.suspension;
+        this.brake;
+    }
+}
