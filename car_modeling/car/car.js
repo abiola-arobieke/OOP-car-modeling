0400514107b35dcb556f3cb2002fad9b60efb853
abiola <abiola.arobieke@gmail.com>
2024-09-10 01:58:52 -0700

    Add a method to add suspsension to class

--- car_modeling/car/car.js
+++ car_modeling/car/car.js
@@ -1,11 +1,11 @@
-import { GearBox } from "../gear/gear_box.js";
-
-export class Car extends GearBox {
-    constructor(year, gearRatio, currentGear) {
-        super(gearRatio, currentGear)
+export class Car {
+    constructor(year, gearBox, engine) {
         this.chasisNumber = Math.floor(Math.random() * 1000) + 1;
         this.year = year
+        this.gearBox = gearBox
+        this.engine = engine
         this.model;
+        this.suspensions = []
     }
 
     get car_model() {
@@ -17,5 +17,10 @@ export class Car extends GearBox {
         if (!this.model.cars.includes(this)) {
             this.model.cars.push(this)
         }
+    }
+
+    addSuspension(suspension){
+        this.suspensions.push(suspension)
+        suspension.car = this
     }
 }
