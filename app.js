c0d85def4448718a1295c64d17263ace7e2a2f47
abiola <abiola.arobieke@gmail.com>
2024-09-10 04:53:32 -0700

    Update app.js file to test classes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import { GearBox } from "./car_modeling/gear/gear_box.js";
 import { GearBoxType } from "./car_modeling/gear/gear_box_type.js"
 import { Engine } from "./car_modeling/car/engine.js";
 import { Suspension } from "./car_modeling/car/suspension.js";
+import { Body } from "./car_modeling/car/body.js";
+import { Wheel } from "./car_modeling/car/wheel.js";
+import { Brake } from "./car_modeling/car/brake.js";
 
 
 let accord = new CarModel("accord")
@@ -16,12 +19,21 @@ let gear_box = new GearBox(19.56, 3)
 let vt2 = new Engine(10.7, 4)
 let vt4 = new Engine(12.8, 6)
 
-let susp1 = new Suspension(34.8)
-let susp2 = new Suspension(30.7)
-let susp3 = new Suspension(32.8)
+let wheel1 = new Wheel(5.2, 3.5, 40)
+let wheel2 = new Wheel(5.2, 3.0, 45)
 
-let eod = new Car(2003, gear_box, vt2)
-let baby_boy = new Car(1998, gear_box, vt4)
+let susp1 = new Suspension(34.8, wheel1)
+let susp2 = new Suspension(30.7, wheel2)
+let susp3 = new Suspension(32.8, wheel1)
+
+let crossTour = new Body(4)
+let sport = new Body(2)
+
+let eod = new Car(2003, gear_box, vt2, crossTour)
+let baby_boy = new Car(1998, gear_box, vt4, sport)
+
+let brake1 = new Brake('drum', eod, wheel1)
+let brake2 = new Brake('disc', baby_boy, wheel2)
 
 eod.addSuspension(susp1)
 eod.addSuspension(susp2)
@@ -36,11 +48,14 @@ gear_box.addGearBoxType(gear_box_type2)
 accord.add_car(eod)
 accord.add_car(baby_boy)
 
-console.log('Suspension 1: ', susp1.car)
+console.log(brake1)
+console.log(brake2)
 
-console.log(baby_boy)
-console.log(eod)
 
+// console.log(baby_boy)
+// console.log(eod)
+
+// console.log('Suspension 1: ', susp1.car)
 // console.log('Gear Box Types: ', gear_box.types)
 // console.log('Gear box of type: 1', gear_box_type1.gearBox)
 
