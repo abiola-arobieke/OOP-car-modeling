f134284c33118921729f31b0dd8778710e8e8814
abiola <abiola.arobieke@gmail.com>
2024-09-10 07:25:51 -0700

    Add shiftUp and shiftDown method

--- car_modeling/gear/gear_box.js
+++ car_modeling/gear/gear_box.js
@@ -1,20 +1,34 @@
 export class GearBox {
-    constructor(gearRatio, currentGear){
+    constructor(gearRatio, currentGear) {
         this.gearRatio = gearRatio
         this.currentGear = currentGear
         this.types = []
     }
 
-    get gearBoxType(){
+    get gearBoxType() {
         return this.types
     }
 
-    set gearBoxType(gearBoxType){
+    set gearBoxType(gearBoxType) {
         this.types = gearBoxType
     }
 
-    addGearBoxType(type){
+    addGearBoxType(type) {
         this.types.push(type)
         type.gearBox = this
+    }
+
+    shiftUp() {
+        if (1 > this.currentGear < 5) {
+            this.currentGear += 1
+            return `You are driving with gear: ${this.currentGear}`
+        }
+    }
+
+    shiftDown() {
+        if (1 > this.currentGear < 5) {
+            this.currentGear -= 1
+            return `You are driving with gear: ${this.currentGear}`
+        }
     }
 }
