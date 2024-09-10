0411a07f046129929d1b44cb26bedc7ca6989a7f
abiola <abiola.arobieke@gmail.com>
2024-09-10 07:26:38 -0700

    Modify gear_box_type.js file

--- car_modeling/gear/gear_box_type.js
+++ car_modeling/gear/gear_box_type.js
@@ -1,17 +1,17 @@
 export class GearBoxType {
-    constructor(name, remarks){
+    constructor(name, remarks) {
         this.name = name
         this.remarks = remarks
         this.gearBoxes;
     }
 
-    get gearBox(){
+    get gearBox() {
         return this.gearBoxes
     }
 
-    set gearBox(gearBox){
+    set gearBox(gearBox) {
         this.gearBoxes = gearBox
-        if(!gearBox.types.includes(this)){
+        if (!gearBox.types.includes(this)) {
             gearBox.types.push(this)
         }
     }
